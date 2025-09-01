213d291d7f8de8abf723bfd5a81684fe2da94b1d
yola <miorayola07@gmail.com>
2025-09-01 14:26:26 +0200

    feat: update and delete

--- K1_toDoList/demo/src/main/java/com/example/demo/service/ToDoService.java
+++ K1_toDoList/demo/src/main/java/com/example/demo/service/ToDoService.java
@@ -29,5 +29,27 @@ public class ToDoService {
     public ToDo update(ToDo todo) {
         return repository.save(todo);
     }
+
+    public ToDo update(Integer id, ToDo updatedTodo) {
+        return repository.findById(id)
+                .map(todo -> {
+                    todo.setTitle(updatedTodo.getTitle());
+                    todo.setDescription(updatedTodo.getDescription());
+                    todo.setStartDateTime(updatedTodo.getStartDateTime());
+                    todo.setEndDateTime(updatedTodo.getEndDateTime());
+                    todo.setDone(updatedTodo.isDone());
+                    return repository.save(todo);
+                })
+                .orElse(null);
+    }
+
+    public boolean delete(Integer id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 
